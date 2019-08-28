3ed57d333fa1f4f0c4c3a7b5b5190a88e36f726e
lamhaoyin2 <46623537+lamhaoyin2@users.noreply.github.com>
2019-08-28 11:16:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import requests
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
@@ -9,4 +10,4 @@ class Redirect(BaseHTTPRequestHandler):
        self.send_header('Location', r.text[r.text.find('vjvars.vdo_url') + 18:r.text[r.text.find('vjvars.vdo_url') + 18:].find('"') + r.text.find('vjvars.vdo_url') + 18])
        self.end_headers()
 
-HTTPServer(("", 80, Redirect)).serve_forever()
+HTTPServer(("", environ['PORT'], Redirect)).serve_forever()
